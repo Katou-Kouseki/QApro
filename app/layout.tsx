00335d4d67249f2bf8e9a217a8662a6119a9fe37
yesmore <3224266014@qq.com>
2023-03-31 20:21:39 +0800

    upd: use tag as version number

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,10 @@ import { ACCESS_CODES, IS_IN_DOCKER } from "./api/access";
 
 let COMMIT_ID: string | undefined;
 try {
-  COMMIT_ID = process.execSync("git rev-parse --short HEAD").toString().trim();
+  COMMIT_ID = process
+    .execSync("git describe --tags --abbrev=0")
+    .toString()
+    .trim();
 } catch (e) {
   console.error("No git or not from git repo.");
 }
